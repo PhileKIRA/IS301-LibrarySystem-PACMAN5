f5a706119196e88b31d2a9d89395d5513cd9d92c
Glen2026 <nambahinglen@gmail.com>
2026-05-15 16:06:51 +1000

    Add borrowing.h header for book borrowing system

--- include/borrowing.h
+++ include/borrowing.h
@@ -1,1 +1,24 @@
+#ifndef BORROWING_H
+#define BORROWING_H
+
+#include "books.h"
+#include "members.h"
+
+#define MAX_BORROWS 1000
+
+typedef struct {
+    char recordID[20];
+    char bookID[20];
+    char memberID[20];
+    char borrowedDate[20];
+    char returnDate[20];
+    char status[20];
+} BorrowRecord;
+
+int issueBook(Book books[], int bookCount, Member members[], int memberCount, BorrowRecord records[], int *recordCount);
+int returnBook(Book books[], int bookCount, BorrowRecord records[], int recordCount);
+int displayBorrowRecords(BorrowRecord records[], int count);
+int displayMemberActivity(Member members[], int memberCount, BorrowRecord records[], int recordCount);
+
+#endif
 
