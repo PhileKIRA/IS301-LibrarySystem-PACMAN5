db1327707ec4d69e08a3257ad9a6398c41af2d6e
Philemon Kira <128330478+PhileKIRA@users.noreply.github.com>
2026-05-15 16:31:41 +1000

    Add file handling function declarations

--- include/filehandling.h
+++ include/filehandling.h
@@ -1,1 +1,20 @@
+#ifndef FILEHANDLING_H
+#define FILEHANDLING_H
+
+#include "books.h"
+#include "members.h"
+#include "borrowing.h"
+
+int loadBooks(Book books[], const char *filename);
+int saveBooks(Book books[], int count, const char *filename);
+int loadMembers(Member members[], const char *filename);
+int saveMembers(Member members[], int count, const char *filename);
+int loadBorrowRecords(BorrowRecord records[], const char *filename);
+int saveBorrowRecords(BorrowRecord records[], int count, const char *filename);
+void removeNewline(char *str);
+int isBackCommand(const char *str);
+int readTextOrBack(const char *prompt, char *buffer, int size);
+int readIntOrBack(const char *prompt, int *value);
+
+#endif
 
